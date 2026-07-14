b32351271903edd93684bb032fed23e6a7aca080
AsifMulla123 <mullaayesha1411@gmail.com>
2026-07-14 15:45:14 +0530

    added user entity and role enum

---
+++ src/main/java/com/learn_spring/shorturl/entity/Role.java
@@ -0,0 +1,5 @@
+package com.learn_spring.shorturl.entity;
+
+public enum Role {
+    USER, ADMIN
+}
--- src/main/java/com/learn_spring/shorturl/entity/User.java
+++ src/main/java/com/learn_spring/shorturl/entity/User.java
@@ -1,4 +1,46 @@
 package com.learn_spring.shorturl.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    Long id;
+
+    @Column(name = "first_name", nullable = false)
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "email", unique = true, nullable = false, length = 255)
+    String email;
+
+    @Column(name = "password", nullable = false)
+    String password;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
